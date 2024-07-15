dda264a1469a2091ddf2712ab9a8aa576e0bdabe
Kovács Roland <kovacs.roland@protecta.hu>
2024-07-15 11:35:27 +0200

    RF: Rudak controller into an Entity class
    The Rudak controller has been redesigned as an Entity class to organize and separate concerns better. By dividing functionality into different methods, we improve code readability and maintainability. It also increases flexibility and robustness by adding validation before database operations.

---
+++ app/Controller/Entities/Rudak.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace App\Controller\Entities;
+
+use App\Controller\Interfaces\EntityInterface;
+use App\Controller\Traits\Response;
+use App\Database\Mysql;
+use Exception;
+
+class Rudak extends AbstractEntity implements EntityInterface
+{
+    use Response;
+    protected const TABLE_NAME = 'rudak';
+    protected const STRUCTURE_SCHEMA = [
+        'name_hu',
+        'name_en',
+        'db',
+        'hossz',
+        'kep',
+    ];
+    protected Mysql $mysql;
+    private object $getParameters;
+
+    public function __construct(object $parameters)
+    {
+        $this->mysql = new Mysql();
+        $this->getParameters = $parameters;
+    }
+
+    public function gelAll(): false|string
+    {
+        try {
+            $sql = "
+                SELECT *
+                FROM " . self::TABLE_NAME . "
+            ";
+
+            $result = $this->mysql->queryObject($sql);
+
+            return $this->jsonResponse($result);
+        } catch (Exception $exception) {
+            return $this->getExceptionFormat($exception->getMessage());
+        }
+    }
+
+    public function get(): false|string
+    {
+        try {
+            if (!isset($this->getParameters->id) && !empty($this->getParameters->id)) {
+                throw new Exception('Missing id parameter or empty');
+            }
+
+            $sql = "
+                SELECT *
+                FROM " . self::TABLE_NAME . "
+                WHERE id = " . $this->getParameters->id . "
+            ";
+
+            $result = $this->mysql->queryObject($sql);
+
+            return $this->jsonResponse($result);
+        } catch (Exception $exception) {
+            return $this->getExceptionFormat($exception->getMessage());
+        }
+    }
+
+    /**
+     * @return false|string
+     */
+    public function create(): false|string
+    {
+        try {
+            $validated = $this->validate(self::STRUCTURE_SCHEMA, $_POST);
+
+            $result = $this->insertData(self::TABLE_NAME,self::STRUCTURE_SCHEMA, $validated);
+
+            if (empty($result)) {
+                return $this->jsonResponse([
+                    'status' => 'error',
+                    'message' => "Rudat nem lehet elmenteni."
+                ]);
+            }
+            return $this->jsonResponse([
+                'status' => 'success'
+            ]);
+        } catch (Exception $exception) {
+            return $this->getExceptionFormat($exception->getMessage());
+        }
+    }
+
+    public function update(): false|string
+    {
+        try {
+            if (!isset($_POST['id'])) {
+                throw new Exception('Missing id parameter or empty');
+            }
+
+            $validated = $this->validate(self::STRUCTURE_SCHEMA, $_POST);
+            $validated->id = $_POST['id'];
+
+            $this->updateData(self::TABLE_NAME,self::STRUCTURE_SCHEMA, $validated);
+
+            return $this->jsonResponse([
+                'status' => 'success'
+            ]);
+        } catch (Exception $exception) {
+            return $this->getExceptionFormat($exception->getMessage());
+        }
+    }
+
+    public function delete(): false|string
+    {
+        try {
+            // TODO legvégső esetben kell csak
+        } catch (Exception $exception) {
+            return $this->getExceptionFormat($exception->getMessage());
+        }
+    }
+}
